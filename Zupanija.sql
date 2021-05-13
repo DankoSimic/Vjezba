d96c0fad350c9e204e41018e5fb21a11c3356368
Danko Šimić <83290695+DankoSimic@users.noreply.github.com>
2021-05-13 15:52:11 +0200

    Zupanija - values #2
    Drugi blok podataka

--- Zupanija.sql
+++ Zupanija.sql
@@ -2,16 +2,22 @@ DROP database IF EXISTS zupanija;
 CREATE database zupanija;
 use zupanija;
 
+# dodan atribut broj stanovnika
 create table zupanija (
 	id_zupanija int primary key auto_increment not null,
 	naziv varchar (50) not null,
-	zupan int
+	zupan int,
+	stanovnistvo decimal (18,0)
 );
 
+# dodan atribut stranka, datum rodjenja i da li je osuđen za kazneno djelo
 create table zupan (
 	id_zupan int primary key auto_increment not null,
 	ime varchar (50) not null,
-	prezime varchar (50) not null
+	prezime varchar (50) not null,
+	stranka varchar (50) not null,
+	datum_rodjenja datetime,
+	osuđivan boolean
 );
 
 create table opcina (
@@ -31,20 +37,21 @@ alter table opcina add foreign key (zupanija) references zupanija (id_zupanija);
 alter table naselje add foreign key (opcina) references opcina (id_opcina);
 
 # id_zupan 1 - 3
-insert into zupan (ime,prezime) values
-('Ivan','Anušić'),
-('Božo','Galić'),
-('Danijel','Marušić')
+# update na temelju novih atributa
+insert into zupan (ime,prezime,stranka,datum_rodjenja,osuđivan) values
+('Ivan','Anušić','HDZ','1973-10-13',true),
+('Božo','Galić','HDZ','1952-08-30',false),
+('Danijel','Marušić','HDZ','1972-01-01',false)
 ;
 
 # id_zupanija 1 - 3
-insert into zupanija (naziv,zupan) values
-('Osječko-baranjska',1),
-('Vukovarsko-srijemska',2),
-('Brodsko-posavska',3)
+insert into zupanija (naziv,stanovnistvo,zupan) values
+('Osječko-baranjska',305032,1),
+('Vukovarsko-srijemska',179521,2),
+('Brodsko-posavska',158575,3)
 ;
 
-# id_opcina 1 - 8
+# id_opcina 1 - 9
 insert into opcina (naziv,zupanija) values
 ('Grad Osijek',1),
 ('Antunovac',1),
@@ -53,7 +60,8 @@ insert into opcina (naziv,zupanija) values
 ('Okučani',3),
 ('Velika Kopanica',3),
 ('Big Digging',3),
-('Little Shovel',3)
+('Little Shovel',3),
+('Schtoppeldorf',1)
 ;
 
 # id_naselje 1 - 17
@@ -93,4 +101,7 @@ where id_naselje=6;
 update naselje set naziv='Sarvaš'
 where id_naselje =7;
 
+# brisanje nepostojecih opcina
 delete from opcina where id_opcina>6;
+
+select * from zupan;
